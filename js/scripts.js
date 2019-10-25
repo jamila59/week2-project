07ed359ac5620bfb9ff056bd15fdb37ea5c2da45
Jamila Aliyeva <padma@email.com>
2019-10-25 11:52:18 -0700

    will make new page responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function check(){
-  var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var correct = 0;
 
+    var question1 = document.quiz.question1.value;
+  	var question2 = document.quiz.question2.value;
+  	var question3 = document.quiz.question3.value;
+  	var correct = 0;
 
 	if (question1 == "mitochondria") {
 		correct++;
