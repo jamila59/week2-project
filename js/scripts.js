8178254d4c72e31a91fec60e9c54e09566a34de2
Jamila Aliyeva <padma@email.com>
2019-10-25 11:29:33 -0700

    will make a new quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var gender = $("input:radio[name=president]:checked").val();
-    var transport = $("#party").val();
-
-    if (gender === 'female'  && transport === '2') {
-      alert("Democrat");
-    } else if (gender === 'female'  && transport === '3') {
-      alert("Liberal");
-    }
-
-    if (gender === 'male'  && transport === '3') {
-      alert("Republican")
-      } else {
-      alert("Undecided");
-    }
+function check(){
+  var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
 
 
-    event.preventDefault();
-  });
-  });
+	if (question1 == "mitochondria") {
+		correct++;
+}
+	if (question2 == "energy") {
+		correct++;
+}
+	if (question3 == "structure") {
+		correct++;
+	}
+
+	var pictures = ["img/borat.jpg", "img/fail.jpeg", "img/okay.jpeg"];
+	var messages = ["Great job!", "That's just okay", "You need to do better"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
