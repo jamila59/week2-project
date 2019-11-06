b8d34717b70ef99949a6794b30e901085354ee9e
Jamila Aliyeva <padma@email.com>
2019-11-05 16:11:51 -0800

    it will change indintation and will make question 5 function

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,44 @@
-// $(document).ready(function() {
-function run(){
-
+unction check(){
 
     var question1 = document.quiz.question1.value;
   	var question2 = document.quiz.question2.value;
   	var question3 = document.quiz.question3.value;
     var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
   	var correct = 0;
-    var incorrect = 0;
+
 
 	if (question1 == "mitochondria") {
 		correct++;
-} else {
-  incorrect--;
-}
+  }
 	if (question2 == "energy") {
 		correct++;
-} else {
-  incorrect--;
-}
+  }
 	if (question3 === "structure") {
 		correct++;
-	} else {
-    incorrect--;
-}
-
-if (question4 == "1") {
-  correct++;
-} else {
-incorrect--;
-}
-
-if (question4 == "3") {
-  correct++;
-} else {
-incorrect--;
-}
-
-
+	}
+  if (question4 == "atp") {
+    correct++;
+  }
+  if (question5 == "3") {
+    correct++;
+  }
 	var pictures = ["img/borat.jpg", "img/fail.jpeg", "img/okay.jpeg"];
-	var messages = ["Great job!", "That's just okay", "You need to do better"];
+	var messages = ["Great job!", "Meh...", "That is okay!"];
 	var score;
 
 	if (correct === 0) {
 		score = 2;
 	}
-
-	if (correct > 0 && correct < 4) {
+	if (correct > 0 && correct < 5) {
 		score = 1;
 	}
-
-	if (correct === 4) {
+	if (correct === 5) {
 		score = 0;
 	}
 
-var correct = 0;
-
-jQuery(document).ready(function() {
-jQuery("#button").click(function() {
-  alert("You got " + correct + " correct.");
-
-});
-});
 	document.getElementById("after_submit").style.visibility = "visible";
 	document.getElementById("message").innerHTML = messages[score];
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 	document.getElementById("picture").src = pictures[score];
-
 	}
